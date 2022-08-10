e0684e4a862eba87a49c7ac5b398fb7c93495868
sbtoonz <67915879+sbtoonz@users.noreply.github.com>
2022-08-09 18:14:20 -0700

    Initial commit

---
+++ ModFrame/ModCore.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using ServerSync;
+
+namespace ModFrame
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class NewMod : BaseUnityPlugin
+    {
+        private const string ModName = "New Mod";
+        private const string ModVersion = "1.0";
+        private const string ModGUID = "some.new.guid";
+        private static Harmony harmony = null!;
+        ConfigSync configSync = new(ModGUID) 
+            { DisplayName = ModName, CurrentVersion = ModVersion, MinimumRequiredVersion = ModVersion};
+        internal static ConfigEntry<bool> ServerConfigLocked = null!;
+        ConfigEntry<T> config<T>(string group, string name, T value, ConfigDescription description, bool synchronizedSetting = true)
+        {
+            ConfigEntry<T> configEntry = Config.Bind(group, name, value, description);
+
+            SyncedConfigEntry<T> syncedConfigEntry = configSync.AddConfigEntry(configEntry);
+            syncedConfigEntry.SynchronizedConfig = synchronizedSetting;
+
+            return configEntry;
+        }
+        ConfigEntry<T> config<T>(string group, string name, T value, string description, bool synchronizedSetting = true) => config(group, name, value, new ConfigDescription(description), synchronizedSetting);
+        public void Awake()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            harmony = new(ModGUID);
+            harmony.PatchAll(assembly);
+            ServerConfigLocked = config("1 - General", "Lock Configuration", true, "If on, the configuration is locked and can be changed by server admins only.");
+            configSync.AddLockingConfigEntry(ServerConfigLocked);
+        }
+    }
+}
